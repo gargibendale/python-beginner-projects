f8ce513c145dd91a649816aa590ea20e7f5ed8d4
gargibendale <gargibendale@gmail.com>
2024-03-18 22:35:27 +0530

    ...

--- madlibs/madlibs.py
+++ madlibs/madlibs.py
@@ -1,1 +1,8 @@
-#hello
+adj = input("Adjective: ")
+verb1 = input("verb1: ")
+verb2 = input("verb2: ")
+famous = input("A celebrity you like: ")
+
+madlib = f"Computer programming is so {adj}! It makes me so excited all the time because \
+I love to {verb1}. Stay hydrated and {verb2} like you are {famous}."
+print(madlib)
