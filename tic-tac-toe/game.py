a0bf1ef22644aee6f3c6659bdeb898a8b7251bdc
gargibendale <gargibendale@gmail.com>
2024-03-20 17:14:23 +0530

    add game and player programs fro tictactoe

---
+++ tic-tac-toe/game.py
@@ -0,0 +1,61 @@
+class TicTacToe:
+    def __init__(self):
+        self.board = [" " for _  in range(9)]
+        self.current_winner = None #keep track of winner!
+    def print_board(self):
+        for row in [self.board[i*3:(i+1)*3] for i in range(3)]:
+            print(' | '+' | '.join(row) + ' | ')
+    @staticmethod
+    def print_board_nums():
+        # 0 | 1 | 2 etc #(tells us what number corresponds to what box)
+        number_board = [[str(i) for i in range(j*3, (j+1)*3)] for j in range(3)]
+        for row in number_board:
+            print(' | '+' | '.join(row) + ' | ')
+    def available_moves(self):
+        moves = []
+        for(i, spot) in enumerate(self.board):
+            # ['x','x','o'] --> [(0,x),(1,x),(2,o)]
+            if spot == ' ':
+                moves.append(i)
+        return moves
+    def empty_squares(self):
+        return ' ' in self.board
+    def num_empty_squares(self):
+        return len(self.available_moves())
+    def make_move(self, square, letter):
+        #if it is a valid move, then make the move,
+        #then return True. if invalid, return False
+        if self.board[square] == ' ':
+            self.board = letter
+            return True
+        return False
+    
+
+def play(game, x_player, o_player, print_game=True):
+    if print_game:
+        game.print_board_nums()
+    letter = 'X' #starting letter
+    #iterate while the game still has empty squares
+    #(we don't have to worry about the winner because we'll just return that which breaks the loop)
+    while game.empty_squares():
+        if letter == 'O':
+            square = o_player.get_move(game)
+        else:
+            square = x_player.get_move(game)
+    if game.make_move(square, letter):
+        if print_game:
+            print(letter + f' makes a move to square {square}')
+            game.print_board()
+            print('') #print an empty line
+        
+        #after we made our move, we need to alternate letters
+        if letter == 'X':
+            letter == 'O'
+        else:
+             letter == 'X'
+        
+
+
+
+
+
